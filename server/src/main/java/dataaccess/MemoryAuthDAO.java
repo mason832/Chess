948ffb55b584b0e6989bca9bcf8cdbb13637eb80
mason832 <mason@cragun.net>
2025-10-19 16:16:33 -0600

    implement phase 3 register endpoint, create dataaccess for authToken info

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,32 @@
 package dataaccess;
+import model.AuthData;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MemoryAuthDAO {
+public class MemoryAuthDAO implements AuthDAO {
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+
+    //add new authorization token
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    //get authorization token
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return authTokens.get(authToken);
+    }
+
+    //delete specific authorization token
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authTokens.remove(authToken);
+    }
+
+    //delete all authorization tokens
+    @Override
+    public void clear() throws DataAccessException {
+        authTokens.clear();
+    }
 }
