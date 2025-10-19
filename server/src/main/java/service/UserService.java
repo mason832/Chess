bd743d45368167bd5a2e59c6d6567bee4f0b4d2d
mason832 <mason@cragun.net>
2025-10-19 16:35:37 -0600

    implement phase 3 register endpoint, create service layer for user info

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,31 @@
 package service;
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import java.util.UUID;
+
 
 public class UserService {
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public AuthData register(UserData user) throws DataAccessException {
+        //make sure all information is provided
+        if (user.username() == null || user.password() == null || user.email() == null) throw new DataAccessException("Error: bad request");
+
+        //make sure username isn't already taken
+        if (userDAO.getUser(user.username())!=null) throw new DataAccessException("Error: already taken");
+
+        //create authorization token
+        String authToken = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(authToken, user.username());
+        authDAO.createAuth(auth);
+
+        return auth;
+    }
 }
