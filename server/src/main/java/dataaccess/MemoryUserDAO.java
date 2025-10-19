d235fe381e24157645edb419952bced8f2161b58
mason832 <mason@cragun.net>
2025-10-19 16:10:36 -0600

    implement phase 3 register endpoint, create dataaccess for user info

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,27 @@
 package dataaccess;
+import model.UserData;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MemoryUserDAO {
+public class MemoryUserDAO implements UserDAO{
+    private final Map<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void createUser(UserData user) throws  DataAccessException {
+        //check if user already exists and adds them if not
+        if (users.containsKey(user.username())) throw new DataAccessException("Error: already taken");
+        users.put(user.username(), user);
+    }
+
+    //returns a user based on given username
+    @Override
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+    //deletes all users
+    @Override
+    public void clear() {
+        users.clear();
+    }
 }
