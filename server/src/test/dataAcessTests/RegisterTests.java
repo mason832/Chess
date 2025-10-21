8bde612592c984903cf37dd9ef48d0ea6aab8346
mason832 <mason@cragun.net>
2025-10-21 10:26:10 -0600

    implement phase 3 register tests made and passed

---
+++ server/src/test/dataAcessTests/RegisterTests.java
@@ -0,0 +1,70 @@
+package dataAcessTests;
+
+import dataaccess.DataAccessException;
+import service.UserService;
+import dataaccess.*;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RegisterTests {
+    private UserService userService;
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+
+    @BeforeEach
+    public void setup() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO, authDAO);
+    }
+
+    @Test
+    public void registerSuccess() throws Exception {
+        var user = new UserData("bob", "1234", "bob@email.com");
+        var auth = userService.register(user);
+        assertNotNull(auth.authToken());
+        assertEquals("bob", auth.username());
+    }
+
+    @Test
+    public void registerFailDuplicate() throws Exception {
+        var user = new UserData("bob", "1234", "bob@email.com");
+        userService.register(user);
+        assertThrows(DataAccessException.class, () -> userService.register(user));
+    }
+
+    @Test
+    public void registerFailEmptyUsername() {
+        var userA = new UserData("", "1234", "bob@email.com");
+        var exceptionA = assertThrows(DataAccessException.class, () -> userService.register(userA));
+        assertTrue(exceptionA.getMessage().contains("bad request"));
+
+        var userB = new UserData(null, "1234", "bob@email.com");
+        var exceptionB = assertThrows(DataAccessException.class, () -> userService.register(userB));
+        assertTrue(exceptionB.getMessage().contains("bad request"));
+    }
+
+    @Test
+    public void registerFailEmptyPassword() {
+        var userA = new UserData("bob", "", "bob@email.com");
+        var exceptionA = assertThrows(DataAccessException.class, () -> userService.register(userA));
+        assertTrue(exceptionA.getMessage().contains("bad request"));
+
+        var userB = new UserData(null, null, "bob@email.com");
+        var exceptionB = assertThrows(DataAccessException.class, () -> userService.register(userB));
+        assertTrue(exceptionB.getMessage().contains("bad request"));
+    }
+
+    @Test
+    public void registerFailEmptyEmail() {
+        var userA = new UserData("bob", "1234", "");
+        var exceptionA = assertThrows(DataAccessException.class, () -> userService.register(userA));
+        assertTrue(exceptionA.getMessage().contains("bad request"));
+
+        var userB = new UserData(null, "1234", null);
+        var exceptionB = assertThrows(DataAccessException.class, () -> userService.register(userB));
+        assertTrue(exceptionB.getMessage().contains("bad request"));
+    }
+}
