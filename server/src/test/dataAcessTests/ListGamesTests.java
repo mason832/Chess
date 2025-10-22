add3c60d08b8a0b5515cefe658757c3450dc369b
mason832 <mason@cragun.net>
2025-10-22 08:58:28 -0600

    implement phase 3 listGame tests complete

---
+++ server/src/test/dataAcessTests/ListGamesTests.java
@@ -0,0 +1,47 @@
+package dataAcessTests;
+import dataaccess.*;
+import model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import service.GameService;
+import service.UserService;
+import java.util.Collection;
+
+
+public class ListGamesTests {
+    private UserService userService;
+    private GameService gameService;
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private UserDAO userDAO;
+
+
+    @BeforeEach
+    public void setup() {
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+        userDAO = new MemoryUserDAO();
+        gameService = new GameService(gameDAO, authDAO);
+        userService = new UserService(userDAO, authDAO);
+    }
+
+    @Test
+    public void listGamesSuccess() throws Exception {
+        var authData = userService.register(new UserData("bob", "1234", "bob@email.com"));
+        Collection<GameData> games = gameService.listGames(authData.authToken());
+
+        assertEquals(0, games.size());
+
+        gameDAO.createGame("Test Game 1");
+        gameDAO.createGame("Test Game 2");
+
+        assertEquals(2, games.size());
+    }
+
+    @Test
+    public void listGameFailUnauthorized() {
+        var exception = assertThrows(DataAccessException.class, () -> gameService.listGames("invalid token"));
+        assertTrue(exception.getMessage().contains("unauthorized"));
+    }
+}
