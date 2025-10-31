a82f575a4ea4b2be5e6ff0a025d1b0c597fe2921
mason832 <mason@cragun.net>
2025-10-31 15:32:28 -0600

    phase 4 creating Data Access tests

---
+++ server/src/test/java/dataaccess/DataAccessUserTests.java
@@ -0,0 +1,10 @@
+package dataaccess;
+import dataaccess.*;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DataAccessUserTests {
+}
