a764b8a4532d819c88c5e9a9cb16c7cf46e76b4a
mason832 <mason@cragun.net>
2025-11-10 13:27:42 -0700

    phase 5 prelogin, register partial implementation

--- client/src/main/java/server/ServerFacade.java
+++ client/src/main/java/server/ServerFacade.java
@@ -3,6 +3,7 @@ import com.google.gson.Gson;
 import model.AuthData;
 import model.UserData;
 
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -27,6 +28,24 @@ public class ServerFacade {
         //write Json body
         try (var out = new OutputStreamWriter(conn.getOutputStream())) {
             gson.toJson(user, out);
+        }
+
+        //error handling
+        var status = conn.getResponseCode();
+
+        if (status == 200) {
+            try (var in = new InputStreamReader(conn.getErrorStream())) {
+                return gson.fromJson(in, AuthData.class);
+            }
+        }
+
+        else {
+            String message;
+            try (var error = new InputStreamReader(conn.getErrorStream())) {
+                message = gson.fromJson(error, String.class);
+            } catch (Exception e) {
+                message = "Server returned status " + status;
+            } throw new Exception(message);
         }
     }
 }
