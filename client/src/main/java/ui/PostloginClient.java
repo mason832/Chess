39eeb5ffc2fa3fd925ed95ed57af1632ad1658b6
mason832 <mason@cragun.net>
2025-11-13 08:17:02 -0700

    phase 5 postlogin, createGame complete implementation

--- client/src/main/java/ui/PostloginClient.java
+++ client/src/main/java/ui/PostloginClient.java
@@ -5,12 +5,26 @@ import server.ServerFacade;
 
 public class PostloginClient {
     private AuthData authData;
+    private ServerFacade server;
 
     public PostloginClient(ServerFacade server, AuthData authData) {
      this.authData = authData;
+     this.server = server;
     }
 
-    public void createGame(String[] input) {}
+    public void createGame(String[] input) {
+        if (input.length != 2) {
+            System.out.println("Usage: create <GAME_NAME>");
+            return;
+        }
+
+        try {
+            server.createGame(authData.authToken(), input[1]);
+        }
+        catch (Exception e) {
+            System.out.println("Create game failed: "+e.getMessage());
+        }
+    }
 
     public void help() {
         System.out.println("""
